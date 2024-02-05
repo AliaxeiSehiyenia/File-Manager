7f513747541fa09307223923abc51fe77caa6e61
Aliaksei Serhiyenia <alexeisergienia@gmail.com>
2024-02-05 19:08:11 +0300

    feat: delete async/await for cat

--- src/app/cat.js
+++ src/app/cat.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import {stdout} from 'node:process';
 
-const cat = async (filePath) => {
+const cat = (filePath) => {
     try {
         /^["']/.test(filePath) ? filePath = filePath.slice(1) : null;
         /["']$/.test(filePath) ? filePath = filePath.slice(0, -1) : null;
 
-        const readStream = await fs.createReadStream(filePath, {encoding: 'utf-8'});
+        const readStream = fs.createReadStream(filePath, {encoding: 'utf-8'});
 
-        await readStream.pipe(stdout);
+        readStream.pipe(stdout);
         readStream.on('error', () => {
             console.log('Operation failed');
         });
